52f2c5e0e998faf1691512617fcd98db20294615
Montse Comas <mcomastu@gmail.com>
2021-02-07 17:50:58 +0100

    flatten data to ts dimensions, label each experiment

--- ini_montse/ini_pipeline.py
+++ ini_montse/ini_pipeline.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.io as sio
 import multiprocessing as mp
 from ini_montse.shared_process import freq_filter, discard_channels
+import itertools
 
 PD_N = 128
 T = 1600
@@ -57,7 +58,8 @@ def process_data(data):
     if N_on == 0:
         subsets = [off_med]
     else:
-        subsets = [on_med, off_med]
+        # REMARK: the order of the sessions is inverted (0- off med, 1- on med)
+        subsets = [off_med, on_med]
 
     ts = np.empty(shape=(0, 2, N_MOTIV, TRIALS, T, PD_N))
     for ds in subsets:
@@ -80,8 +82,36 @@ def process_data(data):
     return ts
 
 
-def define_labels(data):
-    return 0
+def define_labels(data, PD=True):
+    """
+    Label the ts array of a patient with an id and a label for the corresponding state
+    :param data: time series in an array of dimensions (med, rg, n_motive, TRIALS, MS, N)
+        PD: bool, True if data corresponds to PD patients, False otherwise
+    :return: array of dimensions (med x rg x n_motiv x trials, MS, N)
+    """
+    if PD:
+        if data.shape[0] > 1:
+            id_list = [['off', 'on'], ['rg1', 'rg2'],
+                       list(np.arange(N_MOTIV).astype(str)), list((np.arange(TRIALS) + 1).astype(str))]
+        else:
+            id_list = [['off'], ['rg1', 'rg2'],
+                       list(np.arange(N_MOTIV).astype(str)), list((np.arange(TRIALS) + 1).astype(str))]
+        n_electrodes = PD_N
+    else:
+        # NOT IMPLEMENTED CASE
+        id_list = [['heal'], ['rg1', 'rg2'],
+                   list(np.arange(N_MOTIV).astype(str)), list((np.arange(TRIALS) + 1).astype(str))]
+        n_electrodes = 0
+
+    ids = list(itertools.product(*id_list))
+    str_ids = np.array(["-".join(comb) for comb in ids]).reshape(-1, 1)
+    str_labels = np.array(["-".join([comb[0], comb[2]]) for comb in ids]).reshape(-1, 1)
+    data_ids = np.concatenate((str_ids, str_labels), axis=1)
+
+    flat_ts = data.reshape(-1, *data.shape[-2:])
+    # TODO: hi ha nans
+
+    return flat_ts, data_ids
 
 
 def build_ts_dataset(subjects=[68], n_motiv=N_MOTIV):
@@ -90,15 +120,16 @@ def build_ts_dataset(subjects=[68], n_motiv=N_MOTIV):
     # i_sub = 62
     # i_sub = 68
     # (68: 3.42 GB (on-off med), 62: 1.46GB (off med))
+    # TODO: parallelize following loop
     for i_sub in subjects:
         # read data for current subject:
         raw_sorted, i_dir = read_data_ici(i_sub, subset='dataSorted')
-        # keep on/off-med trials if exist, reshape to time series:
+        # keep on/off-med trials if exist (swap order), reshape to time series:
         # REMARK: second dimensions (type of experiment) should not be treated as a different class: TODO: 'mask'
         processed = process_data(data=raw_sorted)
         # label time series (create id and label)
-        labeled_series = define_labels(data=processed)
-
+        flat_sub_ts, sub_ids = define_labels(data=processed)
+        # flat_sub_ts.shape, sub_ids.shape
 
     pass
 
