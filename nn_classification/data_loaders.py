fixed interpretability for mlp with silent channels

nn_classification/data_loaders.py
@@ -14,15 +14,28 @@ def subject_nn_data(subject, healthy_subjects, pd_subjects, data_path, pd_dir, h
                                              healthy_dir=healthy_dir, conv=conv))
     np_labels = np.load(processed_labels_path(subject_id=subject, is_pd=is_pd, data_path=data_path,
                                               pd_dir=pd_dir, healthy_dir=healthy_dir))
+    invalid_ch = np.load(processed_data_path(subject_id=subject, is_pd=is_pd, use_silent_channels=False,
+                                             feature_name='silent_channels', data_path=data_path, pd_dir=pd_dir,
+                                             healthy_dir=healthy_dir))
+
+    if conv:
+        aux_invalid_ch = np.repeat(np.array([invalid_ch]), [3], axis=0).reshape(-1)
+        ts_invalid_ch = np.repeat(np.array([aux_invalid_ch]), 3, axis=0) # TODO
+        invalid_mask = False
+    else:
+        invalid_mask = np.tile(invalid_ch[None, None], (input_data.shape[0], input_data.shape[1], 1))
+
     le = preprocessing.LabelEncoder()
     le.fit(np_labels[:, 1])
     targets = le.transform(np_labels[:, 1])
 
     if use_silent_channels:
         if mask_value == '-1':
-            input_data[np.isnan(input_data)] = float(-1)
+            # input_data[np.isnan(input_data)] = float(-1)
+            input_data[invalid_mask] = float(-1)
         if mask_value == '0':
-            input_data[np.isnan(input_data)] = float(0)
+            # input_data[np.isnan(input_data)] = float(0)
+            input_data[invalid_mask] = float(0)
         elif mask_value == 'mean':
             inds = np.where(np.isnan(input_data))
             dim_means = np.nanmean(input_data, axis=2)

nn_interpretability/display_module.py
@@ -8,36 +8,32 @@ import random
 from scipy.interpolate import griddata
 
 
-def importances_results_df(subject, freq_name, cfg, attrs, electrodes_pos, save_tables=False):
+def importances_results_df(subject, freq_name, cfg, attrs, electrodes_pos, silent_chan, save_tables=False):
     mean_attrs = pd.DataFrame(np.array([np.mean(attr, axis=0) for attr in attrs]).T)
     median_attrs = pd.DataFrame(np.array([np.median(attr, axis=0) for attr in attrs]).T)
     mm_attrs = pd.concat([mean_attrs, median_attrs], axis=1)
-    mean_attrs.columns = ['raw_mot_0', 'raw_mot_1', 'raw_mot_2']
     mm_attrs.columns = ['raw_mean_mot_0', 'raw_mean_mot_1', 'raw_mean_mot_2',
                         'raw_median_mot_0', 'raw_median_mot_1', 'raw_median_mot_2']
-    mean_attrs['electrode_id'] = mm_attrs['electrode_id'] = (mean_attrs.index + 1).astype(int)
+    mm_attrs['electrode_id'] = (mean_attrs.index + 1).astype(int)
 
     electrodes_t_names = pd.DataFrame(np.array([electrodes_pos[:, 0], electrodes_pos[:, -1]]).T)
     electrodes_t_names.columns = ['electrode_id', 'electrode_name']
     electrodes_t_names['electrode_id'] = electrodes_t_names['electrode_id'].astype(int)
+    electrodes_t_names['is_silent'] = silent_chan
 
-    mean_attrs_names = mean_attrs.merge(electrodes_t_names, on='electrode_id')
     mm_attrs_names = mm_attrs.merge(electrodes_t_names, on='electrode_id')
 
     target_dfs = []
     for target_state in [0, 1, 2]:
-        mean_attrs_names[f'abs_mot_{target_state}'] = np.abs(mean_attrs_names[f'raw_mot_{target_state}'])
         mm_attrs_names[f'abs_mean_mot_{target_state}'] = np.abs(mm_attrs_names[f'raw_mean_mot_{target_state}'])
         mm_attrs_names[f'abs_median_mot_{target_state}'] = np.abs(mm_attrs_names[f'raw_median_mot_{target_state}'])
 
-        mean_attrs_names[f'perc_mot_{target_state}'] = \
-            mean_attrs_names[f'abs_mot_{target_state}'] / mean_attrs_names[f'abs_mot_{target_state}'].sum()
         mm_attrs_names[f'perc_mean_mot_{target_state}'] = \
             mm_attrs_names[f'abs_mean_mot_{target_state}'] / mm_attrs_names[f'abs_mean_mot_{target_state}'].sum()
         mm_attrs_names[f'perc_median_mot_{target_state}'] = \
             mm_attrs_names[f'abs_median_mot_{target_state}'] / mm_attrs_names[f'abs_median_mot_{target_state}'].sum()
 
-        target_dfs.append(mm_attrs_names[['electrode_id', 'electrode_name',
+        target_dfs.append(mm_attrs_names[['electrode_id', 'electrode_name', 'is_silent',
                                           f'raw_mean_mot_{target_state}',
                                           f'raw_mean_mot_{target_state}',
                                           f'raw_mean_mot_{target_state}',
@@ -87,16 +83,22 @@ def eeg_convex_hull(xs, ys, do_eeg_plot=True):
 def ig_results_loc_scatter_maps(subject, freq_name, cfg, target_dfs, electrodes_pos, method, silent_chan):
 
     for target_state in [0, 1, 2]:
+        # freq_name=FREQ
         # target_state=0
         imp = target_dfs[target_state][['electrode_name', f'perc_mean_mot_{target_state}']]
         electr_coord = pd.DataFrame(np.array([electrodes_pos[:, -1],
                                               electrodes_pos[:, 1].astype(float),
                                               electrodes_pos[:, 2].astype(float)]).T)
-        electr_coord.columns = ['electrode_name', 'x', 'y']
-        imp_coord = imp.merge(electr_coord, on='electrode_name')
 
-        xs = np.array(electr_coord.x).astype(float)
-        ys = np.array(electr_coord.y).astype(float)
+        electr_coord.columns = ['electrode_name', 'x', 'y']
+
+        imp_coord = pd.merge(imp.assign(str_name=imp.electrode_name.astype(str)),
+                             electr_coord.assign(str_name=electr_coord.electrode_name.astype(str)),
+                             how='inner', on='str_name')[['str_name', 'x', 'y', f'perc_mean_mot_{target_state}']]
+        imp_coord.columns = ['electrode_name', 'x', 'y', f'perc_mean_mot_{target_state}']
+
+        xs = np.array(imp_coord.x).astype(float)
+        ys = np.array(imp_coord.y).astype(float)
         zs = np.array(imp_coord[f'perc_mean_mot_{target_state}']).astype(float)
         xs_s = np.array(electr_coord.x).astype(float)[silent_chan]
         ys_s = np.array(electr_coord.y).astype(float)[silent_chan]
@@ -118,8 +120,10 @@ def ig_results_loc_surf_maps(subject, freq_name, cfg, target_dfs, electrodes_pos
     electr_coord = pd.DataFrame(np.array([electrodes_pos[:, -1],
                                           electrodes_pos[:, 1].astype(float),
                                           electrodes_pos[:, 2].astype(float)]).T)
-    electr_coord.columns = ['electrode_name', 'x', 'y']
+    electr_coord['is_silent'] = silent_chan
+    electr_coord.columns = ['electrode_name', 'x', 'y', 'is_silent']
 
+    # freq_name=FREQ
     # target_state=0
     for target_state in [0, 1, 2]:
         mean_imp = target_dfs[target_state][['electrode_name', f'perc_mean_mot_{target_state}']]
@@ -129,16 +133,22 @@ def ig_results_loc_surf_maps(subject, freq_name, cfg, target_dfs, electrodes_pos
         # i=0, imp=mean_imp
         for i, imp in enumerate([mean_imp, median_imp]):
             measure = measure_names[i]
-            imp_coord = imp.merge(electr_coord, on='electrode_name')
+            imp_coord = pd.merge(imp.assign(str_name=imp.electrode_name.astype(str)),
+                                 electr_coord.assign(str_name=electr_coord.electrode_name.astype(str)),
+                                 how='inner', on='str_name')[['str_name', 'is_silent', 'x', 'y',
+                                                              f'perc_{measure}_mot_{target_state}']]
+            imp_coord.columns = ['electrode_name', 'is_silent', 'x', 'y', f'perc_{measure}_mot_{target_state}']
 
-            x = np.array(electr_coord.x).astype(float)
-            y = np.array(electr_coord.y).astype(float)
+            x = np.array(imp_coord.x).astype(float)
+            xs = np.array(imp_coord[imp_coord.is_silent].x).astype(float)
+            y = np.array(imp_coord.y).astype(float)
+            ys = np.array(imp_coord[imp_coord.is_silent].y).astype(float)
             z = np.array(imp_coord[f'perc_{measure}_mot_{target_state}']).astype(float)
 
             # target grid to interpolate to
             grid_x, grid_y = np.mgrid[-2:2:100j, -2:2:200j]
             points = np.array([x, y]).T
-            silent_points = points[silent_chan]
+            silent_points = np.array([xs, ys]).T
             values = z
 
             # interpolate
@@ -149,6 +159,7 @@ def ig_results_loc_surf_maps(subject, freq_name, cfg, target_dfs, electrodes_pos
             plt.imshow(zi.T, extent=(-2.1, 2.1, -2.1, 2.1), origin='lower')
             plt.plot(points[:, 0], points[:, 1], 'x', ms=5, c='grey')
             plt.plot(silent_points[:, 0], silent_points[:, 1], 'o', ms=5, c='white')
+            plt.colorbar()
             plt.title(f'MLP: F.{measure} Importance with {method} \nSubject {subject} - '
                       f'Target state {target_state} - Freq {freq_name}')
             plt.savefig(osp.join(cfg['outputs_path'], cfg['interpret_figures'], cfg['interpret_loc'],

nn_interpretability/subj_apply_interpret.py
@@ -66,33 +66,30 @@ def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_do
 
         # ---- LOAD TRAINED MODEL ----
         if nn_use_silent_channels:
-            subject_path = f'exp_logs_subject/subject-{subject}/'
+            subject_path = f'exp_logs_subject_fixsilent/subject-{subject}/'
         else:
             subject_path = f'exp_logs_subject_nosilent/subject-{subject}/'
 
         # ---- LOAD TRAIN VAL DATASET ----
         subject_data = SingleSubjectNNData(subject=subject, classifier='mlp', cfg=cfg,
                                            read_silent_channels=True, force_read_split=True)
-        subject_red_data = SingleSubjectNNData(subject=subject, classifier='mlp', cfg=cfg,
-                                               read_silent_channels=False, force_read_split=True)
 
         for FREQ in freq_ids.keys():
             # FREQ = 'gamma'
-            model_ckpt = f'ss-{FREQ}-mlp-pow' if nn_use_silent_channels else f'ss-{FREQ}-mlp-pow-nosilent'
+            model_ckpt = f'ss-{FREQ}-mlp-pow-nosilent' if nn_use_silent_channels else f'ss-{FREQ}-mlp-pow-nosilent'
             ckpt_path = ckpt_paths[subject][model_ckpt]
             model = load_model(osp.join(subject_path, ckpt_path), model='mlp')
 
             freq_id = freq_ids[FREQ]
             train_loader, val_loader = subject_data.mlp_ds_loaders(freq=freq_id)  # 0 alpha, 1 beta, 2 gamma
-            train_loader_red, val_loader_red = subject_red_data.mlp_ds_loaders(freq=freq_id)  # 0 alpha, 1 beta, 2 gamma
 
-            batch, batch_red = next(iter(val_loader)), next(iter(val_loader_red))
+            batch = next(iter(val_loader))
             inputs, targets = batch
-            inputs_red, targets_red = batch_red
-            silent_chan = np.sum(inputs.numpy(), axis=0) == 0
+            silent_chan = np.load(osp.join(cfg['data_path'], cfg['healthy_dir'], f'res_subject_{subject}',
+                                           f'silent-channels-{subject}.npy'))
 
             # ---- MAKE PREDICTIONS ----
-            test_input_tensor = inputs.float().clone() if nn_use_silent_channels else inputs_red.float().clone()
+            test_input_tensor = inputs.float().clone()
             out_probs = model(test_input_tensor).detach().numpy()
             out_classes = np.argmax(out_probs, axis=1)
 
@@ -113,7 +110,7 @@ def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_do
                                          create_histograms=draw_histograms,
                                          silent_chan=silent_chan)
 
-            target_dfs = importances_results_df(subject, FREQ, cfg, attrs, electrodes_pos, save_tables)
+            target_dfs = importances_results_df(subject, FREQ, cfg, attrs, electrodes_pos, silent_chan, save_tables)
 
             if draw_dots_map:
                 ig_results_loc_scatter_maps(subject, FREQ, cfg, target_dfs, electrodes_pos, method=method,