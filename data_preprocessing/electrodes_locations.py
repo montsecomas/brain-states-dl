b8ddb4b7ae0ac80fc8ae6e3c18e1312be9e518f2
Montse Comas <mcomastu@gmail.com>
2021-06-13 18:03:54 +0200

    locations for PD

--- data_preprocessing/electrodes_locations.py
+++ data_preprocessing/electrodes_locations.py
@@ -7,20 +7,25 @@ from utils.file_utils import load_cfg
 coordinates = 'xy'
 do_surface = False
 cfg = load_cfg()
+sufix_str = '_pd' if cfg['run_pd'] else ''
+# sufix_str = ''
+coords_file = 'GSN128.sfp' if cfg['run_pd'] else 'ActiCap64_LM.lay'
+# coords_file = 'ActiCap64_LM.lay'
 
-filename = osp.join(cfg['outputs_path'], cfg['electrodes_map'], 'ActiCap64_LM.lay')
+filename = osp.join(cfg['outputs_path'], cfg[f'electrodes_map{sufix_str}'], coords_file)
 with open(filename) as f:
     content = f.readlines()
 
 content = [x.strip() for x in content]
 str_content = np.asarray([re.split('\t', line.replace(' ', '')) for line in content])
 
-np.save(osp.join(cfg['outputs_path'], cfg['electrodes_map'], 'xy_coord.npy'), str_content)
+np.save(osp.join(cfg['outputs_path'], cfg[f'electrodes_map{sufix_str}'], 'xy_coord.npy'), str_content)
 xs = str_content[:, 1].astype(float)
 ys = str_content[:, 2].astype(float)
-tags = str_content[:, -1]
+tags = np.array([i[3:] for i in str_content[:, 0]]) if cfg['run_pd'] else str_content[:, -1]
 
-fig, ax = plt.subplots()
+plt.rcParams.update({'font.size': 8})
+fig, ax = plt.subplots(figsize=(8, 8))
 ax.scatter(xs, ys, s=30)
 
 for i, txt in enumerate(tags):
@@ -29,8 +34,3 @@ for i, txt in enumerate(tags):
 ax.set_title('Electrodes locations')
 
 fig.savefig(osp.join(cfg['outputs_path'], cfg['electrodes_map'], 'electrodes_loc.png'))
-
-
-
-
-
