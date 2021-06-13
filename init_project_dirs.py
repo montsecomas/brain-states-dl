c0f8286091ab12e84f197cf417b4c06f7469a09d
Montse Comas <mcomastu@gmail.com>
2021-06-13 18:04:03 +0200

    add dirs

--- init_project_dirs.py
+++ init_project_dirs.py
@@ -4,16 +4,20 @@ from utils.file_utils import load_cfg
 
 cfg = load_cfg()
 
-PATHS = [cfg['data'],
-         osp.join(cfg['data'], cfg['pd_dir']),
-         osp.join(cfg['data'], cfg['healthy_dir']),
+PATHS = [cfg['data_path'],
+         osp.join(cfg['data_path'], cfg['pd_dir']),
+         osp.join(cfg['data_path'], cfg['healthy_dir']),
          cfg['outputs_path'],
          osp.join(cfg['outputs_path'], cfg['splits_path']),
          osp.join(cfg['outputs_path'], cfg['interpret_figures']),
          osp.join(cfg['outputs_path'], cfg['interpret_figures'], cfg['interpret_heatmaps']),
          osp.join(cfg['outputs_path'], cfg['interpret_figures'], cfg['interpret_loc']),
-         osp.join(cfg['outputs_path'], cfg['interpret_imp_mlp'])]
+         osp.join(cfg['outputs_path'], cfg['interpret_figures_pd']),
+         osp.join(cfg['outputs_path'], cfg['interpret_figures_pd'], cfg['interpret_heatmaps']),
+         osp.join(cfg['outputs_path'], cfg['interpret_figures_pd'], cfg['interpret_loc']),
+         osp.join(cfg['outputs_path'], cfg['interpret_imp_mlp']),
+         osp.join(cfg['outputs_path'], cfg['interpret_imp_mlp_pd'])]
 
 for path in PATHS:
     if not osp.exists(path):
-        os.mkdir(cfg['data'])
+        os.mkdir(path)
