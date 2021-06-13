2dc01d59556563822a295f7c4275ace98f395f81
Montse Comas <mcomastu@gmail.com>
2021-06-13 18:04:37 +0200

    adding plots

--- nn_interpretability/subj_apply_interpret.py
+++ nn_interpretability/subj_apply_interpret.py
@@ -4,12 +4,13 @@ import seaborn as sns
 from utils.file_utils import load_model
 import numpy as np
 from nn_classification.data_loaders import SingleSubjectNNData
-from nn_interpretability.display_module import importances_results_df, ig_results_loc_scatter_maps, ig_results_loc_surf_maps
+from nn_interpretability.display_module import importances_results_df_beta, ig_results_loc_scatter_maps, \
+    ig_results_loc_surf_maps, input_power_surf
 import os.path as osp
 
 
 def compute_attributions(method, subject, freq_name, model, input_tensor, heatmaps_path, electrodes_pos,
-                         silent_chan, create_histograms=True):
+                         silent_chan, create_histograms=True, session_sufix=''):
     if method == 'IntegratedGradients':
         interpreter = IntegratedGradients(model)
     elif method == 'ShapleyValueSampling':
@@ -31,52 +32,71 @@ def compute_attributions(method, subject, freq_name, model, input_tensor, heatma
 
     # FEATURE IMPORTANCE HEATMAPS FOR EACH LABEL
     if create_histograms:
+        ncols = 9
+        nrows = np.ceil(attrs[0].shape[1]/ncols).astype(int)
         plt.rcParams.update({'font.size': 8})
         for target_state in [0, 1, 2]:
             attr = attrs[target_state]
             abs_attr = np.abs(attr)
             sample_tot = abs_attr.sum(axis=1)
             attr_perc = abs_attr/sample_tot.reshape(-1, 1)
-            fig = plt.figure(figsize=(15, 9))
+            if session_sufix == '':
+                fig = plt.figure(figsize=(15, 9))
+            else:
+                fig = plt.figure(figsize=(9, 15))
             fig.tight_layout()
             fig.suptitle(f'Features importances on validation set examples using {method}\n'
                          f'Subject {subject} - Motivation {target_state} - Frequency {freq_name}')
             for i in range(1, attr.shape[1]+1):
-                plt.subplot(8, 9, i)
+                plt.subplot(nrows, ncols, i)
                 plt.hist(attr_perc[:, i - 1], bins=20)
-                plt.title(f'{electrodes_pos[i - 1, 5]}')
+                if session_sufix == '':
+                    plt.xlim(xmin=0, xmax=0.25)
+                    plt.title(f'{electrodes_pos[i - 1, 5]}')
+                else:
+                    plt.title(f'{electrodes_pos[i - 1, 0]}')
             fig.tight_layout(pad=1.0)
 
-            plt.savefig(osp.join(heatmaps_path, f'feature_importances_{method}_s{subject}_t{target_state}_{freq_name}.png'))
+            plt.savefig(osp.join(heatmaps_path, f'feature_importances_{method}_s{subject}{session_sufix}'
+                                                f'_t{target_state}_{freq_name}.png'))
             plt.clf()
 
     return attrs
 
 
 def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_dots_map, draw_surf_map,
-                          method='IntegratedGradients', nn_use_silent_channels=True):
+                          draw_surf_pow, method='IntegratedGradients'):
 
     freq_ids = dict({'alpha': 0, 'beta': 1, 'gamma': 2})
-    heatmaps_path = osp.join(cfg['outputs_path'], cfg['interpret_figures'], cfg['interpret_heatmaps'])
+    key_sufix = '_pd' if cfg['run_pd'] else ''
+    heatmaps_path = osp.join(cfg['outputs_path'], cfg[f'interpret_figures{key_sufix}'], cfg['interpret_heatmaps'])
+    if cfg['run_pd']:
+        session = '-on' if cfg['model_on_med'] else '-off'
+        subjects_list = cfg['pd_subjects']
+        inputs_path = cfg['pd_dir']
+    else:
+        session = ''
+        subjects_list = cfg['healthy_subjects']
+        inputs_path = cfg['healthy_dir']
 
-    for subject in cfg['healthy_subjects']:
-        # subject = 25
+    for subject in subjects_list:
+        # subject = 25, healthy
+        # subject = 55, PD
         print('------------------------------------\nSubject', subject,
               '\n------------------------------------')
 
         # ---- LOAD TRAINED MODEL ----
-        if nn_use_silent_channels:
-            subject_path = f'exp_logs_subject_fixsilent/subject-{subject}/'
+        if cfg['run_pd']:
+            subject_path = f'{ckpt_paths["mlp-single-pd-subject-logs"]}/subject-{subject}/'
         else:
-            subject_path = f'exp_logs_subject_nosilent/subject-{subject}/'
+            subject_path = f'{ckpt_paths["mlp-single-healthy-subject-logs"]}/subject-{subject}/'
 
         # ---- LOAD TRAIN VAL DATASET ----
         subject_data = SingleSubjectNNData(subject=subject, classifier='mlp', cfg=cfg,
                                            read_silent_channels=True, force_read_split=True)
-
         for FREQ in freq_ids.keys():
             # FREQ = 'gamma'
-            model_ckpt = f'ss-{FREQ}-mlp-pow-nosilent' if nn_use_silent_channels else f'ss-{FREQ}-mlp-pow-nosilent'
+            model_ckpt = f'ss-{FREQ}-mlp-pow{session}'
             ckpt_path = ckpt_paths[subject][model_ckpt]
             model = load_model(osp.join(subject_path, ckpt_path), model='mlp')
 
@@ -85,7 +105,7 @@ def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_do
 
             batch = next(iter(val_loader))
             inputs, targets = batch
-            silent_chan = np.load(osp.join(cfg['data_path'], cfg['healthy_dir'], f'res_subject_{subject}',
+            silent_chan = np.load(osp.join(cfg['data_path'], inputs_path, f'res_subject_{subject}',
                                            f'silent-channels-{subject}.npy'))
 
             # ---- MAKE PREDICTIONS ----
@@ -96,7 +116,7 @@ def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_do
             print(f"Validation Accuracy freq {FREQ}:", sum(out_classes == targets.numpy()) / len(targets))
 
             # ---- INTERPRETER ----
-            electrodes_pos = np.load(osp.join(cfg['outputs_path'], cfg['electrodes_map'], 'xy_coord.npy'))
+            electrodes_pos = np.load(osp.join(cfg['outputs_path'], cfg[f'electrodes_map{key_sufix}'], 'xy_coord.npy'))
             test_input_tensor.requires_grad_()
 
             # method = 'ShapleyValueSampling'
@@ -108,15 +128,21 @@ def run_attributions_bloc(cfg, ckpt_paths, draw_histograms, save_tables, draw_do
                                          heatmaps_path=heatmaps_path,
                                          electrodes_pos=electrodes_pos,
                                          create_histograms=draw_histograms,
-                                         silent_chan=silent_chan)
+                                         silent_chan=silent_chan,
+                                         session_sufix=session)
 
-            target_dfs = importances_results_df(subject, FREQ, cfg, attrs, electrodes_pos, silent_chan, save_tables)
+            target_dfs = importances_results_df_beta(subject, FREQ, cfg, attrs, electrodes_pos, silent_chan, save_tables,
+                                                     session_sufix=session)
 
-            if draw_dots_map:
+            if draw_dots_map and not cfg['run_pd']:
                 ig_results_loc_scatter_maps(subject, FREQ, cfg, target_dfs, electrodes_pos, method=method,
                                             silent_chan=silent_chan)
 
             if draw_surf_map:
                 ig_results_loc_surf_maps(subject, FREQ, cfg, target_dfs, electrodes_pos, method=method,
-                                         silent_chan=silent_chan)
+                                         silent_chan=silent_chan, session_sufix=session)
+
+            if draw_surf_pow:
+                input_power_surf(subject, FREQ, cfg, test_input_tensor.detach().numpy(), electrodes_pos,
+                                 silent_chan=silent_chan, session_sufix=session)
 
--- nn_interpretability/subj_main_interpret.py
+++ nn_interpretability/subj_main_interpret.py
@@ -8,14 +8,15 @@ from nn_interpretability.subj_apply_interpret import run_attributions_bloc
 if __name__ == '__main__':
     cfg = load_cfg()
     ckpt_paths = load_cfg('nn_interpretability/ckpt_paths.yml')
-    DO_HISTS = False
-    DO_TABLES = True
-    DO_PLOT_MAP = True
+    DO_HISTS = False # "time consuming"
+    DO_TABLES = False
+    DO_PLOT_MAP = False
     DO_SURF_MAP = True
+    DO_SURF_POW = True
     USE_SILENT_CHANNELS = True
     # 'IntegratedGradients', 'ShapleyValueSampling', 'KernelShap', 'Lime'
     METHOD = 'ShapleyValueSampling'
 
     run_attributions_bloc(cfg, ckpt_paths, draw_histograms=DO_HISTS, save_tables=DO_TABLES,
-                          draw_dots_map=DO_PLOT_MAP, draw_surf_map=DO_SURF_MAP, method=METHOD,
-                          nn_use_silent_channels=USE_SILENT_CHANNELS)
+                          draw_dots_map=DO_PLOT_MAP, draw_surf_map=DO_SURF_MAP, draw_surf_pow=DO_SURF_POW,
+                          method=METHOD)
